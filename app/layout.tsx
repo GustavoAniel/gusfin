73e687854f45fa3f8b36a35ed26c108c501c8da1
GustavoAniel <aniel.amtimil@gmail.com>
2024-09-26 17:20:30 -0300

    Adiciona descrição e título

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,17 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Sidebar from "@/components/Sidebar";
+import { Metadata } from "next";
 
 const inter = Inter({
   subsets: ['latin'],
   display: 'swap'
 })
+
+export const metadata: Metadata = {
+  title: 'Gusfin',
+  description: 'Site para ver sua situação financeira',
+}
 
 export default function RootLayout({
   children,
